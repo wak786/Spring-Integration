44368f789c7d92f41cd298b4087a00902c282d71
Wasim Khan <Wasim.Khan@IDeaS.com>
2019-02-04 19:07:08 +0530

    Creating payload and header

---
+++ spring-integration/src/main/java/com/wak/integration/SpringIntegrationApplication.java
@@ -0,0 +1,37 @@
+package com.wak.integration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("key", "value");
+		MessageHeaders headers = new MessageHeaders(map); 
+		Message<String> message = new GenericMessage<String>("Hello World", headers);
+		
+		PrintService printService = new PrintService();
+		printService.print(message);
+	}
+
+}
+
---
+++ spring-integration/src/test/java/com/wak/integration/SpringIntegrationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wak.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
